18e39f65f98897ac1badaac0fcbef73d8b071dd0
siva-r767 <167684678+sivarajesh767@users.noreply.github.com>
2025-11-02 12:54:15 +0530

    Update ProductGrid.js

---
+++ src/components/ProductGrid.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ProductCard from './ProductCard';
+const ProductGrid=({products})=>(
+    <div className="product-grid">
+        {products.map(product=>(
+            <ProductCard key={product.id} product={product}/>
+        ))}
+    </div>
+)
+export default ProductGrid
