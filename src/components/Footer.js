aa640ece300c1d3ba31edfb1e4c2d4c2264b3855
siva-r767 <167684678+sivarajesh767@users.noreply.github.com>
2025-11-02 12:52:51 +0530

    Update Footer.js

---
+++ src/components/Footer.js
@@ -0,0 +1,13 @@
+import React from 'react';
+const Footer=()=>(
+    <Footer className='footer'>
+     <p>&copy; 2025 Brand. All rights reserved.</p>
+     <nav>
+        <a href="/">Home</a>
+        <a href='/shop'>shop</a>
+        <a href='/about'>About</a>
+        <a href='/contact'>Contact</a>
+     </nav>
+    </Footer>
+);
+export default Footer;
