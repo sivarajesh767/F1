f253fdadbcfabf865430da252e78b25ad45c07af
siva-r767 <167684678+sivarajesh767@users.noreply.github.com>
2025-11-02 12:54:38 +0530

    Initialize React application in index.js

---
+++ src/components/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDom from'react-som/client';
+import App from './App';
+import './App.css';
+const root=ReactDom.createRoot(document.getElementById('root'));
+root.render(
+    <React.StrictMode>
+<App/>
+    </React.StrictMode>
+);
